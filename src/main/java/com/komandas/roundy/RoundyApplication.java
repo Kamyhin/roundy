297a974e321cabea993d099cfa05ca4f93d4d6ee
Igor Zaporogtsev <igorzaporogtsev@gmail.com>
2020-02-10 21:27:56 +0200

    initial commit

---
+++ src/main/java/com/komandas/roundy/RoundyApplication.java
@@ -0,0 +1,13 @@
+package com.komandas.roundy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komandas/roundy/RoundyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komandas.roundy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
